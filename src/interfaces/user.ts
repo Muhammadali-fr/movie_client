76e2c92e68c8e58c7b5fcffacaf37c5e344ba7dd
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-01-22 23:59:31 +0500

    zustand

---
+++ src/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    provider: "email" | "google";
+    googleId?: string;
+    createdAt: Date;
+};
