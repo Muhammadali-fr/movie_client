f2b74d06664190242bf912bb2caed15079338931
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-01-25 21:10:10 +0500

    some fix

---
+++ src/app/(root)/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="w-full h-[50px] c-gray">
+            <div className="c-width h-full">
+            </div>
+        </header>
+    );
+};
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+    return (
+        <div className="c-width">
+            <button className="w-75 h-12 font-semibold text-lg rounded-4xl  bg-[#0087fe] text-white cursor-pointer hover:bg-[#1b90f7] transition">open modal</button>
+        </div>
+    );
+}
