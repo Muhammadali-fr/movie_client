ec6925dfb601f21e09301fd3e064ffa0a5edf239
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-02-01 21:33:00 +0500

    some mistake

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,11 +3,12 @@ import { logout } from "@/src/api/services/auth";
 import Modal from "@/src/components/Modal";
 import { useUserStore } from "@/src/store/user.store";
 import { useMutation } from "@tanstack/react-query";
+import Image from "next/image";
 import { useState } from "react";
 
 export default function HomePage() {
     const [openModal, setOpenModal] = useState(false);
-    const {setUser} = useUserStore(s => s)
+    const { setUser } = useUserStore(s => s)
     const logoutQuery = useMutation({
         mutationFn: async () => await logout(),
         onSuccess: (data) => {
@@ -24,6 +25,7 @@ export default function HomePage() {
                     <button onClick={() => logoutQuery.mutate()} className="w-full h-12 font-semibold text-lg rounded-4xl  bg-[#0087fe] text-white cursor-pointer hover:bg-[#1b90f7] transition">Log out</button>
                 </div>
             </Modal>
+            <Image unoptimized  src="https://round-482885090933.s3.eu-north-1.amazonaws.com/f10f3f2a-7f77-47fd-8a53-8494bf348124.webp" alt="Logo" width={150} height={150} />
             <button onClick={() => setOpenModal(true)} className="w-75 h-12 font-semibold text-lg rounded-4xl  bg-[#0087fe] text-white cursor-pointer hover:bg-[#1b90f7] transition">open modal</button>
         </div>
     );
