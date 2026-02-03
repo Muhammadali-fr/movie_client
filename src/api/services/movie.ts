730474ab672713b423e01bdbebdb9b8a100ca1ef
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-02-03 11:24:51 +0500

    fix all

--- src/api/services/movie.ts
+++ src/api/services/movie.ts
@@ -1,13 +1,16 @@
 import { fetcher } from "../fetcher";
+import { cookies } from "next/headers";
 
 // movie/upload 
 export async function uploadMovie(data: { title: string, moviePoster: File }) {
     const fd = new FormData();
     fd.append('title', data.title);
     fd.append('moviePoster', data.moviePoster);
-    
+    const cookieHeader = (await cookies()).getAll().map((cookie) => `${cookie.name}=${cookie.value}`).join('; ');
+
     return fetcher('/movie/upload', {
         method: 'POST',
         body: fd,
+        headers: { cookie: cookieHeader }
     });
-}
+};
--- src/components/store-user/StoreUser.tsx
+++ src/components/store-user/StoreUser.tsx
@@ -5,11 +5,11 @@ import StoreUserQuery from "./StoreUserQuery";
 export default async function StoreUser() {
 
     const cookieHeader = (await cookies()).getAll().map((cookie) => `${cookie.name}=${cookie.value}`).join('; ');
-    const data = await fetch(`http://localhost:8000/auth/profile`, {
+    const user = await fetch(`http://localhost:8000/auth/profile`, {
         headers: { cookie: cookieHeader },
         method: 'GET',
         credentials: 'include',
-    });
-    const { user }: { user: IUser } = await data.json();
+    }).then((res) => res.json())
+
     return <StoreUserQuery user={user} />;
 };
