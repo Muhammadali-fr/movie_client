bc0d666456f1a9187c9dc16a66640ba94b9c6e9a
Muhammadali Jamolov <boburjonr934@gmail.com>
2026-02-07 14:23:03 +0500

    redirect

--- src/components/store-user/StoreUser.tsx
+++ src/components/store-user/StoreUser.tsx
@@ -1,15 +1,23 @@
 import { cookies } from "next/headers";
 import { IUser } from "../../interfaces/user";
 import StoreUserQuery from "./StoreUserQuery";
+import { redirect } from "next/navigation";
 
 export default async function StoreUser() {
 
+    console.log("|loading...")
     const cookieHeader = (await cookies()).getAll().map((cookie) => `${cookie.name}=${cookie.value}`).join('; ');
     const data = await fetch(`http://localhost:8000/auth/profile`, {
         headers: { cookie: cookieHeader },
         method: 'GET',
         credentials: 'include',
     });
+
+    console.log("loading endded")
+
+    if (!data.ok || data.status === 401 || data.status === 403 || data.status === 404) {
+        redirect('/auth/sign-in');
+    };
 
     const user: IUser = await data.json();
     return <StoreUserQuery user={user} />;
